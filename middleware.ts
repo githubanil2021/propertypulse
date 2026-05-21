b9ce6c3ba74d3969210b99bd370a9f983deb64bd
githubanil2021 <akumar.contact@gmail.com>
2026-05-21 09:35:03 +0530

    Fix proxy export

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,15 @@
-export { default } from "next-auth/middleware";
+/*export { default } from "next-auth/middleware";
 
 export const config = {
   matcher: ["/profile", "/properties/add"],
 };
+
+*/
+
+
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export function middleware(request: NextRequest) {
+  return NextResponse.next();
+}
